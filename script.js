0a452c731de37a66e8916259519ca0411dacb8f5
AgataSalika <erkaeva-agata@mail.ru>
2025-03-24 12:37:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const input = document.querySelector(".input");
+const hint = document.querySelector(".hint");
+const wordList = document.querySelector(".wordList");
+
+const words = [];
+
+// запасная функция
+
+const renderWords = () => {
+  let wordsStr = words.join("_");
+  wordList.innerHTML = wordsStr;
+};
+
+const renderWords2 = () => {
+  wordList.textContent = "";
+  for (let word of words) {
+    const li = `<li class='word'> ${word} </li>`;
+    wordList.insertAdjacentHTML("beforeend", li);
+  }
+};
+
+function getLastLetter() {
+  const lastWord = words[words.length - 1];
+  let lastLetter = lastWord[lastWord.length - 1];
+  if (lastLetter === "ь" || lastLetter === "ъ" || lastLetter === "ы") {
+    lastLetter = lastWord[lastWord.length - 2];
+  }
+
+  hint.innerHTML = `Слово должно начинаться с буквы <span>${lastLetter}</span> `;
+  return lastLetter;
+}
+
+const checkWord = () => {
+  const checkInput = input.value !== "";
+
+  if (words.length === 0) {
+    if (checkInput) {
+      words.push(input.value.toLowerCase());
+      getLastLetter();
+    }
+  } else if (words.length > 0) {
+    if (checkInput) {
+      const last = getLastLetter();
+      const firstLetter = input.value[0].toLowerCase();
+      if (words.includes(input.value.toLowerCase())) {
+        hint.innerHTML = "Такое слово уже было";
+      } else {
+        if (last === firstLetter) {
+          words.push(input.value.toLowerCase());
+          getLastLetter();
+        }
+      }
+    }
+  }
+};
+
+input.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    checkWord();
+    input.value = "";
+    renderWords2();
+  }
+});
